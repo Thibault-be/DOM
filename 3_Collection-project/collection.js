d2a9d5952d114fe83b5c72621a4bbbd8bf3b5530
Thibault-be <thibault.dhooghe@gmail.com>
2023-10-30 11:55:43 +0100

    create collection

---
+++ 3_Collection-project/collection.js
@@ -0,0 +1,88 @@
+//Videogames
+
+//item: videogame
+//properties: platform, release date, genre, developer, publisher
+
+
+const videogames = [
+  {
+    name: "Devil May Cry",
+    platforms: ["PS2", "PS3", "Android", "NvidiaShield"],
+    release: 2001,
+    genre: ["Hack 'n' Slash", "Action / Adventure"],
+    developer: "Capcom",
+    publisher: "Capcom",
+  },
+  {
+    name: "God of War",
+    platforms: ["PS2", "PS3", "PS4", "PS5"],
+    release: 2005,
+    genre: ["Action / Adventure"],
+    developer: "Santa Monica Studio",
+    publisher: "Sony Computer Entertainment",
+  },
+  {
+    name: "Killzone",
+    platforms: ["PS2", "PS3", "PS4"],
+    release: 2004,
+    genre: ["First-Person Shooter"],
+    developer: "Guerrilla Games",
+    publisher: "Sony Computer Entertainment",
+  },
+  {
+    name: "Socom 2",
+    platforms: ["PS2"],
+    release: 2003,
+    genre: ["Tactical Shooter"],
+    developer: "Zipper Interactive",
+    publisher: "Sony Computer Entertainment",
+  },
+  {
+    name: "Horizon Zero Dawn",
+    platforms: ["PS4", "PS5"],
+    release: 2017,
+    genre: ["Action / RPG"],
+    developer: "Guerrilla Games",
+    publisher: "Sony Interactive Entertainment",
+  },
+  {
+    name: "Ratchet and Clank",
+    platforms: ["PS2", "PS3", "PS4", "PS5"],
+    release: 2002,
+    genre: ["Platformer", "Action / Adventure"],
+    developer: "Insomniac Games",
+    publisher: "Sony Computer Entertainment",
+  },
+  {
+    name: "Call of Duty: Modern Warfare",
+    platforms: ["PS4", "PS5"],
+    release: 2019,
+    genre: ["First-Person Shooter"],
+    developer: "Infinity Ward",
+    publisher: "Activision",
+  },
+  {
+    name: "SSX: Tricky",
+    platforms: ["PS2"],
+    release: 2001,
+    genre: ["Sports (Snowboarding)"],
+    developer: "EA Canada",
+    publisher: "EA Sports BIG",
+  },
+  {
+    name: "The Last of Us",
+    platforms: ["PS3", "PS4"],
+    release: 2013,
+    genre: ["Action / Adventure", "Survival Horror"],
+    developer: "Naughty Dog",
+    publisher: "Sony Computer Entertainment",
+  },
+  {
+    name: "Alan Wake",
+    platforms: ["Xbox 360", "PC"],
+    release: 2010,
+    genre: ["Action / Adventure", "Survival Horror"],
+    developer: "Remedy Entertainment",
+    publisher: "Microsoft Game Studios",
+  },
+];
