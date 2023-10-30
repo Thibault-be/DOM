a91c429b2e836e5adb09b2a672b0b4e0241c9242
Thibault-be <thibault.dhooghe@gmail.com>
2023-10-30 14:42:30 +0100

    finish 3.create exercise

--- 3.Create/script.js
+++ 3.Create/script.js
@@ -1,14 +1,13 @@
-// - Modify the [script.js](./script.js) to create a new `<section>`
-//  with a random background-color for each learner in your group.
-//  This section should contain a paragraph with the name of the learner.
-//  Those sections should be appended in the `<article>`
-// - If the background is dark the text should be white, 
-// if the background is light the text should be black
-// - Find a way so that everytime you load the page the order of the 
-// elements changes!
+const students = ["Danté", "Killian", "Monday", "Josué",  "Thibault", "Luis",
+     "Sieglinde", "Kelsey", "Thierry", "Jonasi", "Becca"];
 
-
-const students = ["Danté", "Killian", "Monday", "Josué", "Thibault", "Luis"]
+//randomise the students list
+for (let i = students.length -1; i > 0; i--) { 
+  let j = Math.floor(Math.random() * (i+1));
+  let k = students[i];
+  students[i] = students[j];
+  students[j] = k;
+}
 
 function randomColor(){
   let min = 0;
@@ -23,12 +22,8 @@ function randomColor(){
   luminance = 0.2126 * (R / 255) + 0.7152 * (G / 255) + 0.0722 * (B / 255)
   console.log(`L is ${luminance}`)
 
-  return colorAndLuminance = [color, luminance]
-
-  
+  return colorAndLuminance = [color, luminance]  
 };
-
-
 
 const newSection = document.createElement("section");
 
