398128102f470b7eb102d77904292d9062c793c1
Thibault-be <thibault.dhooghe@gmail.com>
2023-11-06 16:11:37 +0100

    finish calculator 1.0. All found bugs fixed. More testing probably needed

--- 6_Calculator-project/calculator.js
+++ 6_Calculator-project/calculator.js
@@ -1,3 +1,8 @@
+/***************************************
+start of creation of buttons and display
+****************************************/
+
+//arrays to create the buttons
 const numbers = [0,1,2,3,4,5,6,7,8,9];
 const operators =["plus","minus","multiply","divide"];
 const comma = ["comma"]
@@ -5,55 +10,36 @@ const cBtn = ["C"]
 const equal = ["equal"];
 const negative = ["negative"];
 
+//grab <main> to append all newly created elements
 const mainElement = document.querySelector("main");
 
-
-// const createSpecialButtons = (array, arrayName) =>{
-//   array.forEach(button =>{
-//     const newBtn = document.createElement("button");
-//     const className = `btn-${button.toString()} ${arrayName}`
-//     newBtn.setAttribute("class", className);
-//     newBtn.textContent = button;
-//     mainElement.appendChild(newBtn);
-//   });
-// };
-
+//function to create the buttons
 const createSpecialButtons = (array, arrayName) =>{
   array.forEach(button =>{
     const newBtn = document.createElement("button");
-    //const className = `${button} ${arrayName}`;
     const className = `btn-${button.toString()} ${arrayName}`
     newBtn.setAttribute("class", className);
-
     let specialButtonTextContent = textContentSpecial(button);
     newBtn.textContent = specialButtonTextContent;
     mainElement.appendChild(newBtn);
   });
 };
 
+//function to determin textContent of the button
 const textContentSpecial = (buttonName) =>{
-
   switch (buttonName){
     case "plus": return "+"
-
-    case "negative":
+    case "negative": return "+/-"
     case "minus": return "-"
-    
     case "divide": return "/"
-    
     case "multiply": return "*"
-    
     case "comma": return "."
-    
     case "equal": return "="
-
     default: return buttonName
+  };
+};
 
-  }
-
-
-}
-
+//run the function on all arrays to create the buttons
 createSpecialButtons(numbers, "number");
 createSpecialButtons(operators, "operator");
 createSpecialButtons(comma, "comma");
@@ -61,116 +47,44 @@ createSpecialButtons(cBtn, "cBtn");
 createSpecialButtons(equal, "equals");
 createSpecialButtons(negative, "negative");
 
-
+//make a display for the calculator
 const screen = document.createElement("div");
 screen.setAttribute("class", "screen");
 mainElement.appendChild(screen);
-
-
 const display = document.createElement("div");
 display.setAttribute("class", "display");
 mainElement.insertBefore(display, mainElement.firstElementChild);
 
-//available throughout script
+/***************************************
+end of creation of buttons and display
+****************************************/
+
+
+/*Elements and variables available throughout script 
+****************************************************/
 const screenElement = document.querySelector(".display");
+//keep track of the last button that was clicked
+let lastPressed = "";
+
+
+
+/*add eventlisteners to all the buttons
+***************************************/
 
 //select all numbers and add eventlistener
 const digits = document.querySelectorAll(".number");
 digits.forEach(digit =>{
   digit.addEventListener("click", () =>{
-
     digitEnter(digit.textContent);
   });
 });
-
-//what is shown on the display when a digit is entered?
- const digitEnter = (digit) =>{
-
-      
-   screenContent = screenElement.textContent;
-   let digitScreenValue = digitCheckScreenValue(screenContent, digit)
-   screenElement.textContent = digitScreenValue;
-};
-const digitCheckScreenValue = (string, digit) =>{
-  let lastChar = string.charAt(string.length-1);
-
-  switch (lastChar){
-   case "": {
-    return digit;
-   };
-   case "0":
-   case "1":
-   case "2":
-   case "3":
-   case "4":
-   case "5":
-   case "6":
-   case "7":
-   case "8":
-   case "9":
-   case "+":
-   case "*":
-   case "/":
-   case "-":  {
-    if(string === "0"){
-      return digit;
-    }else{
-      string += digit;
-      return string;
-    };
-   };    
-  }
-};
-
-function deleteOneCharacter(string){
-  let stringArray = Array.from(string);
-  let poppedArray = stringArray.pop();
-  string = stringArray.join("");
-  return string;
-
-};
-
-//delete button removes one character off the display
-const cDeleteBtn = document.querySelector(".cBtn");
-cDeleteBtn.addEventListener("click", () =>{
-
-    screenElement.textContent = deleteOneCharacter(screenElement.textContent);
-  });
-
-//for operations with negative values 
-////////WILL FAIL WHEN LAST CHARACTER IS AN OPERATOR
-const negativeBtn = document.querySelector(".negative");
-negativeBtn.addEventListener("click", () =>{
-  let string = screenElement.textContent;
-  let lastChar = string.charAt(string.length-1);
-  
-  switch(lastChar){
-    case "":{
-      screenElement.textContent = "-"
-      break
-    };
-    case ".":{
-      break
-    };
-    case "-":
-    case "+":
-    case "*":
-    case "/":{
-      string += "-"
-    };
-
-    
-  }
-  
-
-});
-
 
 const operatorBtns = document.querySelectorAll(".operator");
 operatorBtns.forEach(btn =>{
   btn.addEventListener("click", () =>{
     let string = screenElement.textContent;
     let lastChar = string.charAt(string.length-1);
+    lastPressed = "";
     
     switch (lastChar){
 
@@ -192,19 +106,186 @@ operatorBtns.forEach(btn =>{
   })
 });
 
+//delete button removes one character off the display
+const cDeleteBtn = document.querySelector(".cBtn");
+cDeleteBtn.addEventListener("click", () =>{
+  if(lastPressed === "="){
+    screenElement.textContent = "";
+  }else{
+    screenElement.textContent = deleteOneCharacter(screenElement.textContent);
+  }
+});
 
+const commaBtn = document.querySelector(".comma");
+commaBtn.addEventListener("click", () =>{
+  let comma = addComma();
+});
+
+const negativeBtn = document.querySelector(".negative");
+negativeBtn.addEventListener("click", () =>{
+  let makeNegativeResult = makeNegative() 
+});
 
 const equalsBtn = document.querySelector(".equals");
 equalsBtn.addEventListener("click", () =>{
-  screenElement.textContent = computeResult(screenElement.textContent);
+  let result = computeResult(screenElement.textContent);
+  lastPressed = "="
+  screenElement.textContent = result;
 });
 
-///make array with result and what is to be displayed
-function computeResult(str){
-  return Function('return ' + str)()
+
+
+/* Functions to handle input from the buttons
+*********************************************/
+
+//what is shown on the display when a digit is entered?
+ const digitEnter = (digit) =>{
+   let screenContent = screenElement.textContent;
+   let digitScreenValue = digitCheckScreenValue(screenContent, digit)
+   screenElement.textContent = digitScreenValue;
 };
 
-// nul mag niet na operator tenzij daarna een komma komt
+const digitCheckScreenValue = (string, digit) =>{
+  if (lastPressed === "="){
+    lastPressed = "";
+    return digit;
+  }else{
+    let lastChar = string.charAt(string.length-1);
 
-//switchcase for last char can go in separate function
-// also obtain textContent of display can go in a separate function
+    switch (lastChar){
+    case "": {
+      return digit;
+    };
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "+":
+    case "*":
+    case "/":
+    case "-":
+    case ".":  {
+      if(string === "0"){
+        return digit;
+      }else{
+        string += digit;
+        return string;
+      };
+    };    
+    }
+  }
+};
+
+function deleteOneCharacter(string){
+  let stringArray = Array.from(string);
+  let poppedArray = stringArray.pop();
+  string = stringArray.join("");
+  return string;
+
+};
+
+function computeResult(str){
+  return Function('return ' + str)()
+}
+
+function addComma(){
+  
+if (lastPressed != "="){
+  let string = screenElement.textContent;
+  let lastChar = string.charAt(string.length-1);
+
+  //can't have two commas in one operand
+
+  //split the string in several values.
+  commaArray = string.split(/[+\-*\/]/);
+  
+  // on the last item of the array, check if we can add a comma
+  let lastCommaArrayItem = commaArray[commaArray.length-1];
+
+  let commaPresent = lastCommaArrayItem.indexOf(".");
+  console.log(commaPresent)
+  if (commaPresent === -1){
+    string += ".";
+    screenElement.textContent = string;
+  }
+}
+
+  // let string = screenElement.textContent;
+  // let lastChar = string.charAt(string.length-1);
+
+  // //can't have two commas in one operand
+
+  // //split the string in several values.
+  // commaArray = string.split(/[+\-*\/]/);
+  
+  // // on the last item of the array, check if we can add a comma
+  // let lastCommaArrayItem = commaArray[commaArray.length-1];
+
+  // let commaPresent = lastCommaArrayItem.indexOf(".");
+  // console.log(commaPresent)
+  // if (commaPresent === -1){
+  //   string += ".";
+  //   screenElement.textContent = string;
+  // }
+};
+
+
+function makeNegative(){
+
+  //only to work as very first character, or right after operator
+  
+  if(lastPressed=== "="){
+    lastPressed = "";
+    screenElement.textContent = "-"
+  }else{
+    let string = screenElement.textContent;
+  let lastChar = string.charAt(string.length-1);
+  
+
+  if (string.length === 1 && lastChar === "-"){
+    console.log(string.length)
+    console.log("hallo")
+    screenElement.textContent = ""
+  }else {
+    switch (lastChar){
+      case "+":
+      case "-":
+      case "*":
+      case "/":{
+        let penultimateChar = string.charAt(string.length-2);
+        console.log("penultimate", penultimateChar)
+        switch(penultimateChar){
+          case "+":
+          case "-":
+          case "*":
+          case "/":{
+            break;
+          }default: {
+            string += "-"
+            screenElement.textContent = string;
+          }
+          
+        }
+
+      };
+      case ".":{
+        break
+      }
+      case "":{
+        screenElement.textContent = "-";
+        break
+      }
+    }
+  }
+
+  }
+
+  
+  
+};
